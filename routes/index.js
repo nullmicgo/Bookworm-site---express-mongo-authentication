9bcf56feca6a20ab022d0665544d9053df1b6360
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-09 16:00:27 +0800

    Adding Login handling for password

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,23 @@ router.get('/login', function(req,res,next){
 });
 
 router.post('/login', function(req, res,next){
-
+    if(req.body.email && req.body.password){
+        User.authenticate(req.body.email, req.body.password, function(error, user){
+          if(error||!user){
+              var err = new error('Wrong email or password');
+              err.status = 401;
+              return next(err);
+          }
+          else{
+              req.session.userId = user._id;
+              return res.redirect('/profile');
+          }
+        })
+    }else{
+        var err = new Error('Email and password are required');
+        err.status = 401;
+        return next(err);
+    }
 });
 
 
