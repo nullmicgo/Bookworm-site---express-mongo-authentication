2bfe0016395cafedac9bdf79fd60485979f828ca
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-09 15:10:29 +0800

    Bug Fix
    Bug Fix

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ UserSchema.pre('save', function(next){
             if(err){
                 return next(err);
             }
-            user.pasword = hash;
+            user.password = hash;
             next();
      })
 });
