b3c1281733210b051bbaa21292fb6fc66c0bab10
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-09 15:07:52 +0800

    Adding Bcrypt into project
    applying encrypt password process in 'preSave hook'

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -22,6 +22,16 @@ var UserSchema = new mongoose.Schema({
     required: true
   }
 });
-
+//hash password before saving to database
+UserSchema.pre('save', function(next){
+     var user = this;
+     bcrypt.hash(user.password, 10, function(err, hash){
+            if(err){
+                return next(err);
+            }
+            user.pasword = hash;
+            next();
+     })
+});
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
