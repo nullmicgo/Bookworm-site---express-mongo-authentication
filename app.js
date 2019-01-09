ce9214d24bb142cdac4d26f04c454defa1b1f6a8
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-09 17:06:14 +0800

    adding Session Variable

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.use(session({
     saveUninitialized: false
 }));
 
-
+//make user ID available in templates
+app.use(function (req, res, next){
+    res.locals.currentUser = req.session.userId;
+    next();
+});
 
 //mongodb connection
 mongoose.connect("mongodb://localhost:27017/bookworm", { useNewUrlParser: true })
