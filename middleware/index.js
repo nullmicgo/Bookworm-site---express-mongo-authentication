a59271d8437cf3cfeb9b5c41694c9cd2657ba9c5
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-09 19:43:28 +0800

    Adding more middleware for view login content
    also fixed UI issue

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,18 @@
-function loggedOut(req, res, next){
-    if(req.session && req.session.userId){
-        return res.redirect('/profile');
+function loggedOut(req, res, next) {
+    if (req.session && req.session.userId) {
+      return res.redirect('/profile');
     }
     return next();
-}
-module.exports.loggedOut = loggedOut;
+  }
+  function requiresLogin(req, res, next) {
+    if (req.session && req.session.userId) {
+      return next();
+    } else {
+      var err = new Error('You must be logged in to view this page.');
+      err.status = 401;
+      return next(err);
+    }
+  }
+  module.exports.loggedOut = loggedOut;
+  module.exports.requiresLogin = requiresLogin;
+  
